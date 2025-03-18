05294e25ec36c41de6e432d4c4de0b86597fc339
Minozisss <kudryavtsev.ma.03@gmail.com>
2025-03-18 11:29:52 +0300

    Initial Commit

---
+++ NewsTestOne/NewsTestOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsTestOneApp.swift
+//  NewsTestOne
+//
+//  Created by Максим Кудрявцев on 18.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsTestOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
